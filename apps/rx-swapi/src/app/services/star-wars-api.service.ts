6a7cc6db334a2471e65b6c72b82103fd958761c4
xocomil <xocomil@gmail.com>
2023-05-18 19:18:51 -0600

    added autocomplete

--- apps/rx-swapi/src/app/services/star-wars-api.service.ts
+++ apps/rx-swapi/src/app/services/star-wars-api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { create } from 'mutative';
-import { Observable, map, of, switchMap, throwError } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { ApiResponse } from '../models/api-response';
 
 const API_ROOT = 'https://swapi.tech/api';
@@ -14,23 +14,11 @@ export class StarWarsApiService {
   readonly #httpClient = inject(HttpClient);
 
   getPeople(url = PEOPLE_URL): Observable<ApiResponse> {
-    console.log('getting people');
-
-    return of(url).pipe(
-      switchMap((url) => {
-        console.log('in the switchMap');
-
-        if (Math.random() < 0.5) {
-          return throwError(() => 'Random error added for testing purposes.');
-        }
-
-        return this.#httpClient.get<ApiResponse>(url).pipe(
-          map((response) => {
-            return create(response, (draft) => {
-              draft.url = url;
-            });
-          })
-        );
+    return this.#httpClient.get<ApiResponse>(url).pipe(
+      map((response) => {
+        return create(response, (draft) => {
+          draft.url = url;
+        });
       })
     );
   }
